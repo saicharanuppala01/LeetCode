0e0cc010d77c3acc471caefd1ceeed712652eaff
saicharanuppala01 <saicharanuppala01@gmail.com>
2026-08-20 20:07:08 +0530

    Time: 3 ms (60.39%), Space: 21.8 MB (10.38%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        seen=set()
+        for i in nums:
+            if i in seen:
+                seen.remove(i)
+            else:
+                seen.add(i)
+        return seen.pop()
